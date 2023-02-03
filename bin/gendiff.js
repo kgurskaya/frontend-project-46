d68bd55f4b9ea231e7fe61d5a74519edf52c0da9
kgurskaya <ksenia@gurskaya.com>
2023-02-04 00:40:11 +0300

    option and arguments added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,13 @@ import { program } from 'commander';
 
 program
     .version('0.0.1')
-    .description(`Compares two configuration files and shows a difference.`);
-
+    .description(`Compares two configuration files and shows a difference.`)
+    .option('-f, --format <type>',   'output format')
+    .argument('<filepath1>', 'filepath1')
+    .argument('<filepath2>', 'filepath2')
+    // .action((filepath1, filepath2,) => {
+    //     console.log('filepath1:', filepath1);
+    //     console.log('filepath2:', filepath2);
+    //   });
 program.parse();
 
